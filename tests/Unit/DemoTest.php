f0ce1907dcc64b2a1f9a622647cf0ec94a29d763
DESKTOP-T186PC2\itsld <itsldqs2103@gmail.com>
2026-06-10 13:21:14 +0700

    chore: rename unit test filename

--- tests/Unit/DemoTest.php
+++ tests/Unit/DemoTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\TestCase;
 
-class DbTest extends TestCase
+class DemoTest extends TestCase
 {
     use RefreshDatabase;
 
